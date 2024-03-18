672500260341bf466046063fb97499e8778a0614
sciart <sciart@gmail.com>
2024-03-18 02:25:28 -0500

    clean up

--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Counter from './components/Counter';
-import Table from './components/Table';
+import StudentTable from './components/StudentTable';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    {/* <Counter /> */}
-    <Table />
+    <StudentTable />
   </React.StrictMode>
 );
 
